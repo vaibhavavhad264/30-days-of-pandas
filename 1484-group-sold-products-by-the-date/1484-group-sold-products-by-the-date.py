0ace8c5b38a459571945176675845263b6a3e67a
vaibhavavhad264 <167162006+vaibhavavhad264@users.noreply.github.com>
2026-08-19 22:07:06 +0530

    Time: 413 ms (5.95%), Space: 68.5 MB (27.65%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.py
@@ -0,0 +1,8 @@
+# 19/08/2026
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    df = activities.groupby(by = 'sell_date').agg(
+        num_sold = ('product', 'nunique'), 
+        products = ('product', lambda x : ','.join(sorted(x.unique())))).reset_index()
+    return df
